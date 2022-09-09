263536a4f0adf7c4cb591f42ebcb5697ce91b82b
Sakib Ahmed <sakib@cloudycamp.com>
2022-09-09 12:49:51 +0600

    first commit

---
+++ problem_solve.py
@@ -0,0 +1,23 @@
+def get_aspected_index(two_pair_values, target_value):
+    result = []
+    list_length = len(two_pair_values)
+    for pair in range(list_length):
+        if type(two_pair_values[pair]) == list:
+            pair_sum = sum(two_pair_values[pair])
+            if target_value == pair_sum:
+                result.append(pair)
+    return result
+
+two_pair_values = [
+[1, 5],
+[9, -7],
+[0, 8],
+[6, 3],
+[4, 11],
+[14, 0],
+[8, 1],
+[4, 9],(5,4)
+]
+target_value = 9
+result = get_aspected_index(two_pair_values, target_value)
+print(result)
